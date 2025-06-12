13b9ff58854b5d04fadc768d04bb79a504beb62b
Mateus Ascacibas Da Silva <mateus.asilva2001@hotmail.com>
2025-06-12 20:10:06 -0300

    Adjustment tests

--- src/test/java/com/mateusascacibas/device_api/DeviceApiApplicationTests.java
+++ src/test/java/com/mateusascacibas/device_api/DeviceApiApplicationTests.java
@@ -3,7 +3,9 @@ package com.mateusascacibas.device_api;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DeviceApiApplicationTests {
 
